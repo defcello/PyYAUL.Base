e3931777197e5aacfef80c24194f160b88aaab2c
John Crawford <jtcrwfrd@gmail.com>
2015-10-21 22:12:48 -0400

    Added new test.

---
+++ pyyaul/base/_test/test_importlib.py
@@ -0,0 +1,21 @@
+"""
+Test module for `pyyaul.base.importlib".
+"""
+
+import unittest
+from unittest import TestCase
+from pyyaul.base.execommon import ROOTPARENTDIR
+
+
+
+
+class Test_fileImportPath(TestCase):
+
+    def setUp(self):
+        from pyyaul.base import importlib
+        self.m = importlib
+
+    def test_basic(self):
+        act = self.m.fileImportPath(ROOTPARENTDIR / 'pyyaul' / 'base' / 'unittest.py')
+        exp = 'pyyaul.base.unittest'
+        self.assertEquals(act, exp)
